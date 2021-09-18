49c21796d4d01d648ec3b7e05c28d0c169ade959
Vishal Kumar <vishal.kumar3@publicissapient.com>
2021-09-18 17:11:48 +0530

    Add partials

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end("<h1>Namaste Nodejs</h1>");
+    return res.render('home',{
+        title:"Home"
+    });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req,res){
-    res.end("<h1> Users profile</h1>");
+    res.render('user_profile',{
+        title:"User Profile"
+    });
 }
