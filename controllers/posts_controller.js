8db59e82eb872d3d52f03eb705f118fc537939a7
Vishal Kumar <vishal.kumar3@publicissapient.com>
2021-10-06 20:27:52 +0530

    delete the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -11,4 +12,23 @@ module.exports.create = function (req, res) {
         }
         return res.redirect('back');//going to homepage as it was originated from there to display the data
     });
+}
+
+module.exports.destroy = function (req, res) {
+    //req.params.id = id of the post
+    Post.findById(req.params.id, function (err, post) {
+        //here post.user gives id of the user as user id is defined in the post schema
+        if (post.user == req.user.id) { //req.user is already present in session cookies while checking authentication
+            //ideally it should be req.user._id but we need string to compare req.user.id
+            //the user who has reuested to delete should be author of that post then only he can delete
+            post.remove();
+
+            Comment.deleteMany({ post: req.params.id }, function (err) { //post id stored in Comment Schema
+                return res.redirect('back');
+            });
+        } else {
+            //if we don't find any post with given id we should throw error but for now going back
+            return res.redirect('back');
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const passport = require('../config/passport-local-strategy');
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsController.create);
+router.get('/delete/:id',passport.checkAuthentication,postsController.destroy);
 
 module.exports = router;
